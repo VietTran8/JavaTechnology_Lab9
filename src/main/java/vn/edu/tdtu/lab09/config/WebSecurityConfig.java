15ae2ac0aff5f3cb693bdcf662cd5641e24300bd
VietTran8 <cubon882003@gmail.com>
2023-11-22 22:36:32 +0700

    52100864_Lab09

---
+++ src/main/java/vn/edu/tdtu/lab09/config/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package vn.edu.tdtu.lab09.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+}
