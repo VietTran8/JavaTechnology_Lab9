b12149e34c3611ede5f7e5f5ed13a0b8ef78b981
VietTran8 <cubon882003@gmail.com>
2023-11-24 19:51:46 +0700

    52100864_Lab10

--- src/main/java/vn/edu/tdtu/lab09/models/UserAccount.java
+++ src/main/java/vn/edu/tdtu/lab09/models/UserAccount.java
@@ -19,4 +19,5 @@ public class UserAccount {
     private String password;
     private String firstName;
     private String lastName;
+    private String role;
 }
--- src/main/java/vn/edu/tdtu/lab09/repositories/UserRepository.java
+++ src/main/java/vn/edu/tdtu/lab09/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.tdtu.lab09.models.UserAccount;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
